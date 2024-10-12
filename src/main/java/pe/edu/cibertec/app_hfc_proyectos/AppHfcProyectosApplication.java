483359e4d44e831d5a9be03ca6435985125ac029
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-12 00:31:18 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_hfc_proyectos/AppHfcProyectosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_hfc_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHfcProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHfcProyectosApplication.class, args);
+	}
+
+}
